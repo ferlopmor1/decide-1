79ea1d50d820c7982023589ecc0a6159d0734471
ferlopmor1 <fernando.lopez.morato23@gmail.com>
2021-02-08 16:36:13 +0000

    Configurando Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,6 +165,10 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL = "https://mypruebasegc.herokuapp.com"
+
+APIS = {}
+
 try:
     from local_settings import *
 except ImportError:
@@ -180,3 +184,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
