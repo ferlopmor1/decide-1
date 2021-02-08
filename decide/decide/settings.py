9c8e1f79c6bbdb3cecf3e7aaf3c03055994d7589
ferlopmor1 <fernando.lopez.morato23@gmail.com>
2021-02-08 16:43:32 +0000

    Configurando Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,9 +165,7 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
-BASEURL = "https://mypruebasegc.herokuapp.com"
-
-APIS = {}
+BASEURL = 'https://mypruebasegc.herokuapp.com'
 
 try:
     from local_settings import *
@@ -184,6 +182,8 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+APIS = {}
 
 import django_heroku
 django_heroku.settings(locals())
